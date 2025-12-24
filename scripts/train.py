ec4ec9819e89e5c256145444d20ae4a8c91d0164
Xiaoyu Jiang <xiaoyu@delta.iiis.io>
2025-12-24 14:42:35 +0800

    no checkpoint

--- scripts/train.py
+++ scripts/train.py
@@ -269,8 +269,8 @@ def main(config: _config.TrainConfig):
             infos = []
         batch = next(data_iter)
 
-        if (step % config.save_interval == 0 and step > start_step) or step == config.num_train_steps - 1:
-            _checkpoints.save_state(checkpoint_manager, train_state, data_loader, step)
+        # if (step % config.save_interval == 0 and step > start_step) or step == config.num_train_steps - 1:
+        #     _checkpoints.save_state(checkpoint_manager, train_state, data_loader, step)
 
     logging.info("Waiting for checkpoint manager to finish")
     checkpoint_manager.wait_until_finished()
